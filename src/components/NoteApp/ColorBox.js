e0a551b3f9002ef818b25571b32a2aba215c92e6
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-01 11:52:08 -0400

    update inline style, create Color let and pass to style as backgroundColor

--- src/components/NoteApp/ColorBox.js
+++ src/components/NoteApp/ColorBox.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
 
 export default class ColorBox extends Component {
-
     render() {
+        let Color = this.props.Color
         return (
-            <div className='color-box' style={{ backgroundColor: 'red' }}>
-
+            <div className='color-box' style={{ backgroundColor:Color, height: '10px', width: '10px' , borderRadius: '50%'}}>
             </div>
         )
     }
