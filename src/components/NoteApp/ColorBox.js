500f0322a0b072265b39926077f9e25748114d5b
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-01 12:41:41 -0400

    create clickHandler and pass to onClick event

--- src/components/NoteApp/ColorBox.js
+++ src/components/NoteApp/ColorBox.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import './../styles/ColorBox.css'
 
 export default class ColorBox extends Component {
+
+    clickHandler(color) {
+        this.props.onColor(color)
+    }
+    
     render() {
         let Color = this.props.Color
         return (
-            <div className='color-box' style={{ backgroundColor:Color, height: '10px', width: '10px' , borderRadius: '50%'}}>
+            <div className='color-box' onClick={this.clickHandler.bind(this, Color)} style={{ backgroundColor:Color }}>
             </div>
         )
     }
