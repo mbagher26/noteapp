dab3fd613e6097cd6eb8c04aebb1907af8700646
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-02 06:29:55 -0400

    update text and style for div tag

--- src/components/NoteApp/Note.js
+++ src/components/NoteApp/Note.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react'
 export default class Note extends Component {
 
     render() {
+        let color = [...this.props.Color]
         return (
-            <div className="card shadow-sm rounded" style={{ backgroundColor: '#fff' }}><p className="card-text p-3">Go To Gym</p></div>
+            <div className="card shadow-sm rounded" style={{ backgroundColor: color }}><p className="card-text p-3">{this.props.Note}</p></div>
         )
     }
 }
