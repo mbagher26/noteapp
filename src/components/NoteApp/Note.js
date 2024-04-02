fd35ab5866ee76774c505a95a71ce5702f57f35f
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-02 07:39:53 -0400

    create removeHandler and placement as onClick event

--- src/components/NoteApp/Note.js
+++ src/components/NoteApp/Note.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react'
 
 export default class Note extends Component {
 
+    removeHamdler(note) {
+        this.props.onRemove(note)
+    }
     render() {
         let color = [...this.props.Color]
         return (
-            <div className="card shadow-sm rounded" style={{ backgroundColor: color }}><p className="card-text p-3">{this.props.Note}</p></div>
+            <div className="card shadow-sm rounded" style={{ backgroundColor: color }} onClick={this.removeHamdler.bind(this, this.props.Note)}><p className="card-text p-3">{this.props.Note}</p></div>
         )
     }
 }
